7fb4cca391c20314436871a5c871cdaf988f189e
Александр <146124268+Raigis@users.noreply.github.com>
2024-09-27 11:15:12 +0000

    Completed Third Task

--- ThirdTask/main3.cpp
+++ ThirdTask/main3.cpp
@@ -7,12 +7,18 @@
 #include <iostream>
 #include <vector>
 
-void print_error () {
-    std::cout << "Введено меньше 5-ти чисел.";
+void print_error (int code) {
+    switch (code) {
+    case 1: 
+        std::cout << "\nFewer than 5 numbers have been entered.\n\n";
+        break;
+    case 2:
+        std::cout << "\nIncorrect input.\n\n";
+    }
 }
 
 void print_back (std::vector<int> &input) {
-    std::cout << "Пятое по возрастанию введённое число: " << input[4] <<".";
+    std::cout << "\nThe fifth highest number entered: " << input[4] <<".\n\n";
 }
 
 void insertion (std::vector<int> &input, int &enter) {
@@ -22,37 +28,54 @@ void insertion (std::vector<int> &input, int &enter) {
         input.push_back(input[0]);
         input[0] = enter;
     } else {
-        for (int i = 0, y = i + 1; y <= input.size(); i++, y++) {
-            if (enter >= input[i] && enter < input[y]) {
+        for (int i = 0; i <= input.size(); i++) {
+            if (enter <= input[i]) {
                 if (input.size() == 5) input.pop_back();
-                auto pos = input.begin();
-                input.insert(std::next(pos, y), enter);
+                auto pos = std::next(input.begin(), i);
+                input.insert(pos, enter);
                 break;
             } 
         }
     }
 }
 
-void check (std::vector<int> &input, int &enter) {
-    if (input.size() != 5 && enter == -1) {
-        print_error();
-    } else if (enter == -1) {
+void conversion (std::vector<int> &input, std::string &enter) {
+    int en;
+    for (int i = 0; i < enter.size(); i++) {
+        if (enter[i] >= '0' && enter[i] <= '9'){
+            i == 0 ? en = enter[i] - '0' : en = en * 10 + (enter[i] - '0');
+        } else {
+            print_error(2);
+            return;
+        }
+    }
+    insertion(input, en);
+}
+
+void check (std::vector<int> &input, std::string &enter) {
+    if (input.size() != 5 && enter == "-1") {
+        print_error(1);
+    } else if (enter == "-1") {
         print_back(input);
     } else {
-        insertion(input, enter);
+        conversion(input, enter);
     }
 }
 
 int main () {
-    std::vector<int> input;
-    int enter;
+    std::vector<int> input (5, 0);
+    input.shrink_to_fit();
+    input.clear();
+    std::string enter;
     do {
-        std::cout << "Введите число: ";
+        std::cout << "Enter a number: ";
         std::cin >> enter;
-        if (enter != -2) {
+        if (enter != "-2") {
             check(input, enter);
         }
-    } while (enter != -2);
+    } while (enter != "-2");
+
+    std::cout << "The work of the program is completed";
 
     return 0;
 }
